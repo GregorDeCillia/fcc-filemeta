f87e92d064fc8c669dbfecfba04da97c8d8358ad
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-05 02:30:18 +0200

    use arrow functions exclusively

--- api/index.js
+++ api/index.js
@@ -7,13 +7,11 @@ var app = express();
 app.use(cors({ origin: 'https://www.freecodecamp.org' }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (_req, res) {
-  res.sendFile(process.cwd() + '/index.html');
-});
+app.get('/', (_req, res) =>
+  res.sendFile(process.cwd() + '/index.html'));
 
-app.get('/style.css', function (_req, res) {
-  res.sendFile(process.cwd() + '/style.css');
-});
+app.get('/style.css', (_req, res) =>
+  res.sendFile(process.cwd() + '/style.css'));
 
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
   res.json({
